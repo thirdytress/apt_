e43e9cfa367b46bba131518d2717478d7efe363b
Jaime Garcia III <garciajx@students.nu-lipa.edu.ph>
2025-09-16 09:13:52 +0800

    updated dbcon

--- database/db.php
+++ database/db.php
@@ -1,15 +1,16 @@
 <?php
-$host = 'localhost';
-$db = 'apartmenthub_db';
-$user = 'root';
-$pass = '';
-
+$host = 'mysql.hostinger.com';
+$db = 'u164511188_apartmenthub';
+$user = 'u164511188_apthub';
+$pass = 'Apartmenthub@01';
+//db password: apartmenthub@01
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+
 
 
 ?>
